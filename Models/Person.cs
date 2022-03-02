0f155828aee94a3a0adc5dc311b84cb0fa18862e
Rajashree <bhuyan.rajashree@gmail.com>
2022-03-03 03:39:05 +0530

    Added two class person and todo class

--- Models/Person.cs
+++ Models/Person.cs
@@ -4,7 +4,51 @@ using System.Text;
 
 namespace Todo_app.Models
 {
-    class Person
+    public class Person
     {
+        private int id;
+        private String firstName;
+        private String lastName;
+
+        public Person(int id, string firstName, string lastName)
+        {
+            if ((!firstName.Equals(null) && !firstName.Equals("") && !lastName.Equals(null) && !lastName.Equals("")) || (id > 0))
+            {
+                this.id = id;
+                this.firstName = firstName;
+                this.lastName = lastName;
+            }
+
+        }
+        public int Id {
+            get => id;
+        }
+        public string FirstName {
+            get => firstName;
+            set
+            {
+                if (!value.Equals(null) && !value.Equals(""))
+                {
+                    firstName = value;
+                }
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                if (!value.Equals(null) && !value.Equals(""))
+                {
+                    lastName = value;
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => $"{firstName} {lastName}";
+        }
+
     }
 }
---
+++ Models/Todo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo_app.Models
+{
+    class Todo
+    {
+        private int id;
+        private string description;
+        private bool done;
+        private Person assignee;
+
+
+        public Todo(int id, string description)
+        {
+           this.id = id;
+           this.description = description;
+        }
+
+        public int Id
+        { 
+            get => id;
+        }
+        
+        public string Description
+        {
+            get { 
+                return description; 
+            }
+            set
+            {
+                if (!value.Equals(null) && !value.Equals(""))
+                {
+                    description = value;
+                }
+            }
+        }
+
+        public Todo(int id, string description, bool done)
+        {
+            this.done = done;
+            this.id = id;
+            this.description = description;
+        }
+    }
+}
