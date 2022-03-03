88e7b840c576f2d69dd5a251ec019745c77f14c9
Rajashree <bhuyan.rajashree@gmail.com>
2022-03-04 02:04:37 +0530

    Added person and todo sequencer class

---
+++ Data/PersonSequencer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo_app.Data
+{
+    class PersonSequencer
+    {
+        private static int personId=0;
+        public static int NextPersonId()
+        {
+            personId = personId + 1;
+            return personId;
+        }
+
+        public static void Reset()
+        {
+            personId = 0;
+        }
+    }
+}
---
+++ Data/TodoSequencer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo_app.Data
+{
+    class TodoSequencer
+    {
+        private static int todoId = 0;
+        public static int NextTodoId()
+        {
+            todoId = todoId + 1;
+            return todoId;
+        }
+
+        public static void Reset()
+        {
+            todoId = 0;
+        }
+    }
+}
